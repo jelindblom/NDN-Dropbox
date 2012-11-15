e50403416eb8161cb4c65f9b9ff2a002a58219b2
Jared <jared@ubuntu.(none)>
2012-11-15 00:28:23 -0800

    Make bin directory if it does not exist

--- compile.sh
+++ compile.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
 
+# Make bin directory
+mkdir ./NDNDropbox/bin 1> /dev/null 2> /dev/null
+
 # Compile Application Source
 javac -d ./NDNDropbox/bin -classpath ./NDNDropbox/libs/ccn.jar:./NDNDropbox/libs/bcprov-jdk16-143.jar:./NDNDropbox/libs/commons-codec-1.7.jar:./NDNDropbox/libs/commons-io-2.4.jar:./NDNDropbox/libs/jnotify-0.94.jar:. ./NDNDropbox/src/*.java
 
 # Group and Compress Binaries
 jar cvfm NDNDropbox.jar ./META-INF/MANIFEST.MF -C ./NDNDropbox/bin .
 
+# Compiled
+echo "Compiled."
