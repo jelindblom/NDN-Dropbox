ede8c81687b87f4025679b3e9e72f1cf41d6f42e
Jared <jared@ubuntu.(none)>
2012-11-15 00:11:01 -0800

    Added the ability to compile and execute application without Eclipse

---
+++ compile.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+# Compile Application Source
+javac -d ./NDNDropbox/bin -classpath ./NDNDropbox/libs/ccn.jar:./NDNDropbox/libs/bcprov-jdk16-143.jar:./NDNDropbox/libs/commons-codec-1.7.jar:./NDNDropbox/libs/commons-io-2.4.jar:./NDNDropbox/libs/jnotify-0.94.jar:. ./NDNDropbox/src/*.java
+
+# Group and Compress Binaries
+jar cvfm NDNDropbox.jar ./META-INF/MANIFEST.MF -C ./NDNDropbox/bin .
+
---
+++ execute.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+# Execute Application
+java -Djava.library.path=./NDNDropbox/libs -jar NDNDropbox.jar
